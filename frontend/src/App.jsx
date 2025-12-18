742590b08309a5c196f590980700f7fc4797f566
Parvej Khan <iamparvejkhan03@gmail.com>
2025-12-18 12:41:59 +0530

    fixed issue 1 in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 
